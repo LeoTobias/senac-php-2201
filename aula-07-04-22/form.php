79a16899b25ba458c9760db8b9261bf4aec9d8a2
LEONARDO TOBIAS <leonardo.tobias@senacsp.edu.br>
2022-04-07 22:12:30 -0300

    Finalização do CRUD

---
+++ aula-07-04-22/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tarefas</title>
+</head>
+<body>
+    <form action="insert.php" method="post">
+        <label>Adicionar uma nova tarefa:</label><br>
+        <input type="text" name="tarefa" placeholder="Digite uma nova tarefa">
+        <input type="submit" name="realizar" value="Enviar"><br>
+    </form>
+</body>
+</html>
